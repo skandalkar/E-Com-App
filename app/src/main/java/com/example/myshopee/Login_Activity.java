cc85ac682c50b9b72c54c712bf1b6ba56e01f278
SANTOSH <skandalkar1508@gmail.com>
2023-06-06 12:36:23 +0530

    init commit

--- app/src/main/java/com/example/myshopee/Login_Activity.java
+++ app/src/main/java/com/example/myshopee/Login_Activity.java
@@ -2,13 +2,46 @@ package com.example.myshopee;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class Login_Activity extends AppCompatActivity {
+public class Login_Activity extends AppCompatActivity
+{
 
+    EditText username,password;
+    Button nxtBtn;
+    ImageView google,facebook;
+    TextView regiHere;
+    @SuppressLint("MissingInflatedId")
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        nxtBtn = findViewById(R.id.nextbtn);
+
+        google = findViewById(R.id.google);
+        facebook = findViewById(R.id.facebook);
+
+        regiHere = findViewById(R.id.registerhere);
+
+        nxtBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(Login_Activity.this,VerifyOTP_Activity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/myshopee/VerifyOTP_Activity.java
@@ -0,0 +1,13 @@
+package com.example.myshopee;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VerifyOTP_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_verify_otp);
+    }
+}
